8d80349936ae68f25f58b08747ba2b9829166884
ROVENSKYI MARKO <rovenskyimarko@MacBook-Pro-ROVENSKYI.local>
2023-05-03 14:22:29 +0300

    first commit

---
+++ demo/Database.php
@@ -0,0 +1,43 @@
+<?php
+
+// connect to the db, and execute a query.
+class Database {
+    public $connection;
+     public $statement;
+    public function __construct($config, $username = 'root', $password = '') {
+
+
+        $dsn = 'mysql:' . http_build_query($config, '',';');
+        /*$dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};user=root;charset={$config['charset']}";*/
+
+        $this -> connection = new PDO($dsn,$username, $password,[
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+    public function query($query, $params = [])
+    {
+        $this->statement = $this ->connection->prepare($query);
+        $this->statement->execute($params);
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+         $result = $this->find();
+         if (! $result) {
+             abort();
+         }
+         return $result;
+    }
+}
---
+++ demo/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'functions.php';
+require 'Database.php';
+require 'Response.php';
+require 'router.php';
+
+
+
+
+/*$id = $_GET['id'];*/
+/*$query = "select * from posts where id = :id";
+
+
+$posts = $db->query($query, [':id' => 1])->fetch();
+
+dd($posts);*/
+
+
+/*class Person {
+      public $name;
+      public  $age;
+
+      public function breathe() {
+          echo $this->name.' is breathing';
+      }
+  }
+
+  $person = new Person();
+
+  $person ->name = ' john doe ';
+  $person ->age  = 25;
+
+$person->breathe();*/
+
+
+
+
